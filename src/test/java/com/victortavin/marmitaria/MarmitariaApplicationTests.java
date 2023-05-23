88a2f311a0bb896c6d2214df9e8b7956b05e12d4
Otavio Marques <marquesotavio251204@gmail.com>
2023-05-23 15:01:00 -0300

    subindo projeto

---
+++ src/test/java/com/victortavin/marmitaria/MarmitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victortavin.marmitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
