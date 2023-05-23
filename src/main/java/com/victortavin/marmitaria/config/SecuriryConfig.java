31ae61aaa883da74d3e64461f33305a056aa8bfd
Otavio Marques <marquesotavio251204@gmail.com>
2023-05-23 15:09:13 -0300

    config

---
+++ src/main/java/com/victortavin/marmitaria/config/SecuriryConfig.java
@@ -0,0 +1,13 @@
+package com.victortavin.marmitaria.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecuriryConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/**");
+    }
+}
