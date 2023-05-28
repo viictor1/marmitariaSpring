568fd9ed6e25086e3fd8d6e59eb1397b90dd6c25
Victor <victorgaspalmeida@hotmail.com>
2023-05-28 10:50:59 -0300

    começo JwtAuthenticationFilter

---
+++ src/main/java/com/victortavin/marmitaria/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package com.victortavin.marmitaria.security.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+	
+	@Override
+	protected void doFilterInternal(@NotNull HttpServletRequest request, 
+			@NotNull HttpServletResponse response, 
+			@NotNull FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String authHeader = request.getHeader("Authorization");
+		final String jwt;
+		 final String email;
+		
+		if(authHeader == null || !authHeader.startsWith("Bearer ") ) {
+			Filter.chain.doFilter(request, response);
+		}
+	}
+
+}
