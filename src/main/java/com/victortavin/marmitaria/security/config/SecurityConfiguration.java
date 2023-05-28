25a082b9f308e4ff4c25cfed2e8cd0a22c71e350
Victor <victorgaspalmeida@hotmail.com>
2023-05-28 12:59:29 -0300

    filtro de acesso dentro do SecurityConfig

---
+++ src/main/java/com/victortavin/marmitaria/security/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.victortavin.marmitaria.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfiguration {
+	
+	private final JwtAuthenticationFilter jwtAuthFilter = new JwtAuthenticationFilter();
+	private final AuthenticationProvider authenticationProvider = null;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/register", "/login").permitAll()
+            .requestMatchers("/roles").hasRole("ADMIN")
+        )
+        .sessionManagement(sessionManagement -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .authenticationProvider(authenticationProvider())
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+}
