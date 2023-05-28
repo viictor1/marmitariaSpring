ab789c0413372243609e28092f42e9e17fb429bd
Victor <victorgaspalmeida@hotmail.com>
2023-05-28 11:09:42 -0300

    JwtService

--- src/main/java/com/victortavin/marmitaria/security/config/JwtService.java
+++ src/main/java/com/victortavin/marmitaria/security/config/JwtService.java
@@ -1,11 +1,43 @@
 package com.victortavin.marmitaria.security.config;
 
+import java.security.Key;
+import java.util.function.Function;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 	
+	private static final String SECRET_KEY = "566D5970337336763979244226452948404D635166546A576E5A723474377721";
+	
 	public String extractUsername(String token) {
-		return null;
+		return extractClaims(token, Claims::getSubject);
 	}
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(getSignInKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+	}
+	
+	private Key getSignInKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
+	public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+		
+	}
+
+	
+	
 }
