7df08987aea3d6aa3750bc5bdae6d1366d78d4e6
Otavio Marques <marquesotavio251204@gmail.com>
2023-06-10 22:34:52 -0300

    pedido de adicionar saldo

--- src/main/java/com/victortavin/marmitaria/filters/TokenFilter.java
+++ src/main/java/com/victortavin/marmitaria/filters/TokenFilter.java
@@ -1,13 +1,18 @@
 package com.victortavin.marmitaria.filters;
 
 import java.io.IOException;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.victortavin.marmitaria.controllers.exceptions.StandardError;
 import com.victortavin.marmitaria.entities.UserEntity;
 import com.victortavin.marmitaria.repositories.UserRepository;
 import com.victortavin.marmitaria.service.TokenService;
@@ -18,6 +23,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
+@EnableAutoConfiguration
 public class TokenFilter extends OncePerRequestFilter{
 
 	@Autowired
@@ -26,18 +32,34 @@ public class TokenFilter extends OncePerRequestFilter{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired  // Precisa disso para criar o mapper que transforma o erro em json 
+    Jackson2ObjectMapperBuilder mapperBuilder;
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
 		var token = recoverToken(request);
 		
-		UserEntity user = null;
 		
+		UserEntity user = null;
 		   if (token == null) {
-		        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // Define o status de resposta como 401 (Unauthorized)
-		        response.getWriter().write("Token não fornecido"); // Envia uma mensagem de erro ao cliente
-		        return;
+			   if(!request.getRequestURI().equals("/users/login")
+					   && !request.getRequestURI().equals("/users/cadastro")
+					   && !request.getRequestURI().equals("/h2-console/**")) {
+				   
+				   	ObjectMapper mapper = mapperBuilder.build();				   
+			        StandardError erro = new StandardError(Instant.now(), 403, "Acces Denied", "Usuário não está autenticado", request.getRequestURI());
+			        String json = mapper.writeValueAsString(erro);
+			        
+			        response.setContentType("application/json");
+			        response.setCharacterEncoding("UTF-8");
+			        response.setStatus(403);
+			        response.getWriter().write(json);
+			        
+			        return;
+			        
+			   } 
 		    }
 		
 		if(token != null) {
@@ -46,7 +68,7 @@ public class TokenFilter extends OncePerRequestFilter{
 			user = userRepository.findByEmail(subjetc);
 			
 			if(user != null) {
-				var authentication = new UsernamePasswordAuthenticationToken(user, user.getId(), user.getAuthorities());
+				var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 				
 				SecurityContextHolder.getContext().setAuthentication(authentication);
 			}
