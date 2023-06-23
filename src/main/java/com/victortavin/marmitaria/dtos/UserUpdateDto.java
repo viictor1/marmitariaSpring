f8439cc5a8fbff17fd4cfa0c258eb93d9a204fe3
Victor <victorgaspalmeida@hotmail.com>
2023-06-23 11:52:45 -0300

    alterar user tudo certinho

---
+++ src/main/java/com/victortavin/marmitaria/dtos/UserUpdateDto.java
@@ -0,0 +1,85 @@
+package com.victortavin.marmitaria.dtos;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UserUpdateDto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String cpf;
+	private String oldPassword;
+	private String newPassword;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getOldPassword() {
+		return oldPassword;
+	}
+
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, firstName, id, lastName, newPassword, oldPassword);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserUpdateDto other = (UserUpdateDto) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(id, other.id) && Objects.equals(lastName, other.lastName)
+				&& Objects.equals(newPassword, other.newPassword) && Objects.equals(oldPassword, other.oldPassword);
+	}
+	
+	
+}
