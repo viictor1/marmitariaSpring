30eee5ac5e06698efce55b9b5a4285c2f350773f
Otavio Marques <marquesotavio251204@gmail.com>
2023-06-23 18:01:06 -0300

    atualizando

--- src/main/java/com/victortavin/marmitaria/entities/DemandsEntity.java
+++ src/main/java/com/victortavin/marmitaria/entities/DemandsEntity.java
@@ -3,6 +3,12 @@ package com.victortavin.marmitaria.entities;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,8 +20,52 @@ public class DemandsEntity implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private double price;
 	private String road;
-	private boolean delivered; 
+	private boolean delivered;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private UserEntity user;
+	
+	public DemandsEntity() {
+		
+	}
+	
+	public DemandsEntity(Long id, double price, String road, boolean delivered) {
+		super();
+		this.id = id;
+		this.price = price;
+		this.road = road;
+		this.delivered = delivered;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getRoad() {
+		return road;
+	}
+	public void setRoad(String road) {
+		this.road = road;
+	}
+	public boolean isDelivered() {
+		return delivered;
+	}
+	public void setDelivered(boolean delivered) {
+		this.delivered = delivered;
+	}
+	
+	
 }
