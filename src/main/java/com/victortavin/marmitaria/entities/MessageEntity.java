47c28c7f6a23d4ba3f990e054ecddab41239f5ab
Otavio Marques <marquesotavio251204@gmail.com>
2023-06-24 12:59:50 -0300

    entity message

---
+++ src/main/java/com/victortavin/marmitaria/entities/MessageEntity.java
@@ -0,0 +1,90 @@
+package com.victortavin.marmitaria.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "messages")
+public class MessageEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String recipient;
+	private String title;
+	private String message;
+	
+	public MessageEntity() {
+		
+	}
+	
+	public MessageEntity(Long id, String recipient, String title, String message) {
+		super();
+		this.id = id;
+		this.recipient = recipient;
+		this.title = title;
+		this.message = message;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRecipient() {
+		return recipient;
+	}
+
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MessageEntity other = (MessageEntity) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
